61bbf4917c653bd68451cd9307e3c0e46548632b
hwolff00 <hwolff00@gmail.com>
2021-07-22 14:07:42 -0400

    Add queue and __str__ methods.

--- stack_queue.py
+++ stack_queue.py
@@ -12,6 +12,18 @@ class Stack:
         self.head = None
         self.popped = None
 
+    def __str__(self):
+        lst = []
+        if not self.head:
+            return '[]'
+        else:
+            node = self.head
+            while node.next_node is not None:
+                lst.append(str(node.data))
+                node = node.next_node
+            lst.append(str(node.data))
+        return f'[{", ".join(lst)}]'
+
     def pop(self):
         if not self.head:
             return "Stack is already empty."
@@ -29,9 +41,7 @@ class Stack:
             self.head = node
 
     def is_empty(self):
-        if not self.head:
-            return True
-        return False
+        return not self.head
 
     def peek(self):
         if not self.head:
@@ -42,12 +52,65 @@ class Stack:
             self.popped = None
 
 
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.removed = None
+
+    def __str__(self):
+        lst = []
+        if not self.head:
+            return '[]'
+        else:
+            node = self.head
+            while node.next_node is not None:
+                lst.append(str(node.data))
+                node = node.next_node
+            lst.append(str(node.data))
+        return f'[{", ".join(lst)}]'
+
+    def add(self, data):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+        else:
+            head = self.head
+            while head.next_node is not None:
+                head = head.next_node
+            head.next_node = node
+
+    def remove(self):
+        if not self.head:
+            return "Queue empty."
+        else:
+            self.removed = self.head
+            self.head = self.head.next_node
+            return self.removed.data
+
+    def peek(self):
+        if not self.head:
+            self.head = self.removed
+        else:
+            self.removed.next_node = self.head
+            self.head = self.removed
+            self.removed = None
+
+    def is_empty(self):
+        return not self.head
+
+
 if __name__ == "__main__":
     pringles = Stack()
     print(pringles.is_empty())
     pringles.push(4)
     pringles.push(7)
+    print(pringles)
     print(pringles.pop())
     print(pringles.pop())
     pringles.peek()
     print(pringles.is_empty())
+    taco_truck = Queue()
+    print(taco_truck.is_empty())
+    taco_truck.add(4)
+    taco_truck.add(7)
+    print(taco_truck)
