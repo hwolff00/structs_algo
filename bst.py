af0d6ceaf49d486ab0c82ce3bcdb200c6ae24e18
hwolff00 <hwolff00@gmail.com>
2021-07-22 19:11:15 -0400

    Add bst node  removal method.

--- bst.py
+++ bst.py
@@ -88,6 +88,58 @@ class BST:
             else:
                 return "Node not in tree."
 
+    def remove(self, data):
+        if not self.root:
+            return "Tree is empty."
+        node = self.root
+        self.remove_node(data, node)
+
+    def get_predecessor(self, node):
+        if node.right_child:
+            return self.get_predecessor(node.right_child)
+        return node
+
+    def remove_node(self, data, node):
+        if node is None:
+            print(f"Node {data} not in tree")
+            return
+        elif data < node.data:
+            self.remove_node(data, node.left_child)
+        elif data > node.data:
+            self.remove_node(data, node.right_child)
+        else:
+            if not node.left_child and not node.right_child:
+                print(f"Removing leaf node {node.data}")
+                if node.parent is not None and node.parent.left_child == node:
+                    node.parent.left_child = None
+                elif node.parent is None:
+                    self.root = None
+                else:
+                    node.parent.right_child = None
+            elif not node.left_child and node.right_child is not None:
+                print(f"Removing {node.data}: has single right child.")
+                if node.parent.left_child == node:
+                    node.parent.left_child = node.right_child
+                elif node.parent is None:
+                    self.root = node.right_child
+                else:
+                    node.parent.right_child = node.right_child
+            elif node.left_child is not None and not node.right_child:
+                print(f"Removing {node.data}: has single left child.")
+                if node.parent.right_child == node:
+                    node.parent.right_child = node.left_child
+                elif node.parent is None:
+                    self.root = node.left_child
+                else:
+                    node.parent.left_child = node.left_child
+            else:
+                print(f"Removing {node.data}: has left and right child.")
+                predecessor = self.get_predecessor(node.left_child)
+                temp_node = predecessor.data
+                predecessor.data = node.data
+                node.data = temp_node
+                self.remove_node(data, predecessor)
+
 
 if __name__ == "__main__":
     tree = BST()
@@ -98,9 +150,8 @@ if __name__ == "__main__":
     tree.insert(12)
     tree.insert(13)
     tree.insert(2)
-    # tree.in_order_traversal()
+    tree.in_order_traversal()
     # tree.post_order_traversal()
-    print(tree.search_bst(4))
-    print(tree.search_bst(13))
-    print(tree.search_bst(6))
-    print(tree.search_bst(20))
+    tree.remove(4)
+    tree.remove(9)
+    tree.in_order_traversal()
