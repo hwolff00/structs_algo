190e5afedef89aec992c22bd92628c6b43e9f358
hwolff00 <hwolff00@gmail.com>
2021-07-24 12:28:21 -0400

    Add more context and better variable names.

--- merge_sort.py
+++ merge_sort.py
@@ -3,36 +3,40 @@
 
 
 def merge_sort(items):
-    if len(items) == 1:
+    # Divide Phase (split into single items)
+    if len(items) == 1:  # base case
         return
     mid = len(items) // 2
     left_half = items[:mid]
     right_half = items[mid:]
+    print(f'items: {items}')
+    print(f'left: {left_half}')
+    print(f'right: {right_half}')
 
     merge_sort(left_half)
     merge_sort(right_half)
 
+    le = 0
+    r = 0
     i = 0
-    j = 0
-    k = 0
-
-    while i < len(left_half) and j < len(right_half):
-        if left_half[i] < right_half[j]:
-            items[k] = left_half[i]
-            i += 1
+    # Conquer Phase
+    while le < len(left_half) and r < len(right_half):
+        if left_half[le] < right_half[r]:
+            items[i] = left_half[le]
+            le += 1
         else:
-            items[k] = right_half[j]
-            j += 1
-        k += 1
-
-    while i < len(left_half):
-        items[k] = left_half[i]
+            items[i] = right_half[r]
+            r += 1
         i += 1
-        k += 1
-    while j < len(right_half):
-        items[k] = right_half[j]
-        j += 1
-        k += 1
+
+    while le < len(left_half):
+        items[i] = left_half[le]
+        le += 1
+        i += 1
+    while r < len(right_half):
+        items[i] = right_half[r]
+        r += 1
+        i += 1
     return items
 
 
